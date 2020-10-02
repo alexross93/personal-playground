56f9b6bed4acb28ee93707bcf8445400b3112c6c
Alex Ross <arossg17@gmail.com>
2020-10-01 21:12:12 -0400

    create quasar app

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-list>
+    <q-item v-for="info in navInfo" :key="info.id" clickable tag="a" target="_blank" :href="info.link">
+      <q-item-section avatar>
+        <q-icon :name="info.icon" />
+      </q-item-section>
+      <q-item-section>{{info.title}}</q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data () {
+    return {
+      navInfo: [
+        { title: 'Personal Site v1', icon: 'construction', link: 'https://alexgrahamross.ca/' },
+        { title: 'Github', icon: 'person', link: 'https://github.com/alexross93' }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>Personal Site</q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <NavBar />
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import NavBar from 'components/NavBar.vue'
+
+export default {
+  name: 'MainLayout',
+  components: { NavBar },
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
